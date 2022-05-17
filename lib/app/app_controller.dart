9af62c1817592cfc28a2442a8cb134474030467c
Rafael Alves da Costa <rafalves.costa@gmail.com>
2022-05-17 16:32:22 -0300

    step-3: services

--- lib/app/app_controller.dart
+++ lib/app/app_controller.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter_architecture_basics/app/interfaces/local_storage_interface.dart';
 import 'package:flutter_architecture_basics/app/models/appconfig_model.dart';
+import 'package:flutter_architecture_basics/app/services/shared_local_storage_service.dart';
 
 class AppController {
   static final AppController instance = AppController._();
-  AppController._();
+  AppController._() {
+    storage.get('isDark').then((value) {
+      if (value != null) {
+        config.themeSwitch.value = value;
+      }
+    });
+  }
 
+  final ILocalStorage storage = SharedLocalStorageService();
   final AppConfigModel config = AppConfigModel();
+
   bool get isDark => config.themeSwitch.value;
   ValueNotifier<bool> get themeSwitch => config.themeSwitch;
 
   changeTheme(bool value) {
-    themeSwitch.value = value;
+    config.themeSwitch.value = value;
+    storage.put('isDark', value);
   }
 }
--- lib/app/pages/home/components/custom_switch_widget.dart
+++ lib/app/pages/home/components/custom_switch_widget.dart
@@ -1,25 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_architecture_basics/app/app_controller.dart';
 
-class CustomSwitchWidget extends StatefulWidget {
+class CustomSwitchWidget extends StatelessWidget {
   const CustomSwitchWidget({Key? key}) : super(key: key);
 
   @override
-  State<CustomSwitchWidget> createState() => _CustomSwitchWidgetState();
-}
-
-class _CustomSwitchWidgetState extends State<CustomSwitchWidget> {
-  bool status = AppController.instance.isDark;
-
-  @override
   Widget build(BuildContext context) {
-    return Switch(
-      value: status,
-      onChanged: (value) {
-        AppController.instance.changeTheme(value);
-        setState(() {
-          status = value;
-        });
+    return ValueListenableBuilder(
+      valueListenable: AppController.instance.themeSwitch,
+      builder: (context, value, _) {
+        return Switch(
+          value: AppController.instance.isDark,
+          onChanged: (value) {
+            AppController.instance.changeTheme(value);
+          },
+        );
       },
     );
   }

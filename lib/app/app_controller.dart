b480d41d403a6fa8f5a9ec5784a6ea0a1b6b4131
Rafael Alves da Costa <rafalves.costa@gmail.com>
2022-05-18 07:05:48 -0300

    step-4: viewmodel

--- lib/app/app_controller.dart
+++ lib/app/app_controller.dart
@@ -1,26 +1,17 @@
 import 'package:flutter/foundation.dart';
-import 'package:flutter_architecture_basics/app/interfaces/local_storage_interface.dart';
-import 'package:flutter_architecture_basics/app/models/appconfig_model.dart';
 import 'package:flutter_architecture_basics/app/services/shared_local_storage_service.dart';
+import 'package:flutter_architecture_basics/app/viewmodels/change_theme_viewmodel.dart';
 
 class AppController {
   static final AppController instance = AppController._();
   AppController._() {
-    storage.get('isDark').then((value) {
-      if (value != null) {
-        config.themeSwitch.value = value;
-      }
-    });
+    changeThemeViewModel.init();
   }
 
-  final ILocalStorage storage = SharedLocalStorageService();
-  final AppConfigModel config = AppConfigModel();
+  final ChangeThemeViewModel changeThemeViewModel =
+      ChangeThemeViewModel(storage: SharedLocalStorageService());
 
-  bool get isDark => config.themeSwitch.value;
-  ValueNotifier<bool> get themeSwitch => config.themeSwitch;
-
-  changeTheme(bool value) {
-    config.themeSwitch.value = value;
-    storage.put('isDark', value);
-  }
+  bool get isDark => changeThemeViewModel.config.themeSwitch.value;
+  ValueNotifier<bool> get themeSwitch =>
+      changeThemeViewModel.config.themeSwitch;
 }
---
+++ lib/app/viewmodels/change_theme_viewmodel.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_architecture_basics/app/interfaces/local_storage_interface.dart';
+import 'package:flutter_architecture_basics/app/models/appconfig_model.dart';
+
+class ChangeThemeViewModel {
+  final ILocalStorage storage;
+
+  final AppConfigModel config = AppConfigModel();
+
+  ChangeThemeViewModel({
+    required this.storage,
+  });
+
+  Future init() async {
+    await storage.get('isDark').then((value) {
+      if (value != null) {
+        config.themeSwitch.value = value;
+      }
+    });
+  }
+
+  changeTheme(bool value) {
+    config.themeSwitch.value = value;
+    storage.put('isDark', value);
+  }
+}

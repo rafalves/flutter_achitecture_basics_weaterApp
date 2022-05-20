87bccf63cf92d2cd6eaa40f65be8091140d13fae
Rafael Alves <rafalves.costa@gmail.com>
2022-05-20 20:46:03 -0300

    Update utils.dart

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,2 +1,2 @@
 const openWheaterUrl =
-    'https://api.openweathermap.org/data/2.5/weather?id=3448439&units=metric&appid=1cb0e771da8e0a61c19f7a193793945d';
+    'https://api.openweathermap.org/data/2.5/weather?id=3448439&units=metric&appid=apitoken';
